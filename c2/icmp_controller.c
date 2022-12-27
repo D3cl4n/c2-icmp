77698814db1d1da1207e76cfe3b93734d65dd641
D3cl4n <dpmurphy@wpi.edu>
2022-12-27 11:30:10 -0500

    working on sending ICMP sequence

--- c2/icmp_controller.c
+++ c2/icmp_controller.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
@@ -8,6 +9,7 @@
 
 
 #define SOCKET_CREATE_ERROR 0x1
+#define TEST_BOT "192.168.239.128"
 
 int main(int argc, char *argv[]) {
     int sockfd;
@@ -22,7 +24,17 @@ int main(int argc, char *argv[]) {
     memset(&serv_addr, 0, sizeof(serv_addr));
     memset(&icmp_hdr, 0, sizeof(icmp_hdr));
 
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr(TEST_BOT);
+    
+    icmp_hdr.type = ICMP_ECHO;
+    icmp_hdr.code = 0;
+    icmp_hdr.un.echo.id = 0;
+    icmp_hdr.un.echo.sequence = 0;
+    icmp_hdr.un.frag.__unused = cpu_to_be16(1234); 
 
+    sendto(sockfd, &icmp_hdr, sizeof(icmp_hdr), 0, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    close(sockfd);
 
     return 0;
 }
